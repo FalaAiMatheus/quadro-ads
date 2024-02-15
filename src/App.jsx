c94e173fafd69bbabf19aecc4145a126e002c5ab
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-15 17:41:06 -0300

    feat: definindo as cores do bg do body e do content e criando o restante das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./routers/Home";
+import Curso from "./routers/Curso";
+import Sobre from "./routers/Sobre";
+import Salvos from "./routers/Salvos";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
-        <main>
+        <main className="flex justify-center mx-auto mt-[2.6875rem] shadow-md w-full max-w-[1379px] bg-white rounded-[1.625rem]">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="curso" element={<Curso />} />
+            <Route path="sobre" element={<Sobre />} />
+            <Route path="salvos" element={<Salvos />} />
           </Routes>
         </main>
       </BrowserRouter>

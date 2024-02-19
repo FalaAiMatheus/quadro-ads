563dd2b0b41acee0f15b66e02cd07277cf0ed724
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-19 11:40:58 -0300

    feat: adicionado o team data

--- src/data.jsx
+++ src/data.jsx
@@ -33,10 +33,33 @@ function data() {
       cadeira: "Projeto de banco de dados",
       professor: "Gerhard",
       sala: 205,
-      horario: ["20:20 - 22:00", 'Sábado: 07:20 - 08:20'],
+      horario: ["20:20 - 22:00", "Sábado: 07:20 - 08:20"],
     },
   ];
-  return database;
+  const teamData = [
+    {
+      id: 1,
+      name: "Matheus França",
+      photo: "../src/assets/photo-matheus.jpg",
+      desc: "Desenvolvedor Front End Júnior @opovoonline, atualmente cursando o terceiro semestre de ADS (Análise e Desenvolvimento de Sistemas) na UNINASSAU.",
+    },
+    {
+      id: 2,
+      name: "João Pedro",
+      photo: "../src/assets/photo-joão.png",
+      desc: "Analista de dados, atualmente cursando o terceiro semestre de ADS (Análise e Desenvolvimento de Sistemas) na UNINASSAU.",
+    },
+    {
+      id: 3,
+      name: "Mateus Micael",
+      photo: "../src/assets/photo-mateus.png",
+      desc: "Sou Mateus Micael, tenho 20 anos e sou entusiasta de qualquer forma de arte, atualmente procuro uma forma de alinhar essa minha paixão a tecnologia para levar isso como uma forma de ser bem sucedido no mercado digital.",
+    },
+  ];
+  return {
+    database,
+    teamData
+  } 
 }
 
 export default data;

40cdd6dfcf7ae2e1b4e131e7d10e8a5bc7907667
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-19 09:37:01 -0300

    feat: finalizando o componente person card e importando as fotos dos integrantes

--- src/components/Card/PersonCard.jsx
+++ src/components/Card/PersonCard.jsx
@@ -1,20 +1,10 @@
-import Photo from "../../assets/Ellipse 1.png";
-
-function PersonCard() {
+function PersonCard({ name, photo, desc }) {
   return (
     <div className="card flex gap-3">
-      <img className="object-contain" src={Photo} alt="" />
+      <img className="object-contain" src={photo} alt="Foto de perfil" />
       <div className="flex flex-col">
-        <span className="font-medium text-3xl">Matheus França</span>
-        <span className="font-medium">
-          Idade: <span className="font-normal">18</span>
-        </span>
-        <p className="max-w-96 text-sm font-medium text-slate-700">
-          Desenvolvedor Front End Júnior{" "}
-          <a href="https://www.opovo.com.br/" target="_blank" rel="external" aria-label="Site do OPOVO" className="underline text-sky-600 hover:text-sky-900 transition">@opovoonline</a>,
-          atualmente cursando o terceiro semestre de ADS (Análise e
-          Desenvolvimento de Sistemas) na UNINASSAU.{" "}
-        </p>
+        <span className="font-medium text-3xl">{name}</span>
+        <p className="max-w-96 text-sm font-medium text-slate-700">{desc}</p>
       </div>
     </div>
   );

30c1f73f36e6db226825df71556eaca2a7dedd0c
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-19 09:38:36 -0300

    feat: criando a logica dos dias de forma dinamica

--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -5,17 +5,34 @@ function Home() {
   const actualDateDay = new Date().getDay();
   const actualDate = new Date().toLocaleDateString("pt-br");
   const [actualDay, setActualDay] = useState("");
-  const [date, setDate] = useState("");
 
   useEffect(() => {
-    function setDay() {
-      if (actualDateDay === 5) {
+    switch (actualDateDay) {
+      case 0:
+        setActualDay("Domingo");
+        break;
+      case 1:
+        setActualDay("Segunda feira");
+        break;
+      case 2:
+        setActualDay("Terça feira");
+        break;
+      case 3:
+        setActualDay("Quarta Feira");
+        break;
+      case 4:
+        setActualDay("Quinta feira");
+        break;
+      case 5:
         setActualDay("Sexta feira");
-        setDate(actualDate);
-      }
+        break;
+      case 6:
+        setActualDay("Sábado");
+        break;
+      default:
+        setActualDay(null);
     }
-    setDay();
-  }, [actualDateDay, actualDate]);
+  }, [actualDateDay]);
 
   const database = [
     {
@@ -38,12 +55,18 @@ function Home() {
     <section className="p-6 w-full space-y-5 animation">
       <div className="flex flex-col font-medium gap-1">
         <span className="text-3xl">Hoje é: {actualDay}</span>
-        <span className="text-base text-slate-400">Dia: {date}</span>
+        <span className="text-base text-slate-400">Dia: {actualDate}</span>
       </div>
       <div className="h-px bg-slate-200 rounded-sm max-w-[765px]" />
       <div className="flex flex-col gap-4">
-        {database.map(({id, cadeira, professor, horario, sala}) => (
-          <LessonCard key={id} cadeira={cadeira} professor={professor} horario={horario} sala={sala} />
+        {database.map(({ id, cadeira, professor, horario, sala }) => (
+          <LessonCard
+            key={id}
+            cadeira={cadeira}
+            professor={professor}
+            horario={horario}
+            sala={sala}
+          />
         ))}
       </div>
     </section>

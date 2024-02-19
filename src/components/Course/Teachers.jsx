6696998ad7d05ddc2fcdc41892a987d737496e6b
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-19 15:07:56 -0300

    feat: adicionando o nome completo

---
+++ src/components/Course/Teachers.jsx
@@ -0,0 +1,21 @@
+import data from "../../data";
+
+function Teachers() {
+  const { database } = data();
+  return (
+    <div className="flex flex-col gap-2">
+      <h2 className="font-medium text-3xl">Professores</h2>
+      <div className="h-px bg-slate-200 rounded-sm max-w-[765px]" />
+      {database.map(({ id, professor, professorFullName }) => (
+        <>
+          <p className="font-medium" key={id}>
+            {professorFullName} ({professor})
+          </p>
+          <div className="h-px w-full bg-slate-200 max-w-96" />
+        </>
+      ))}
+    </div>
+  );
+}
+
+export default Teachers;

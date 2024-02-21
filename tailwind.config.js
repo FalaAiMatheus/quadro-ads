1b1f5e2d469aa955866e5479295c63f1f0d55c76
Francisco Matheus de Sousa França <RH.MatheusFranca@opovo.local>
2024-02-21 10:29:44 -0300

    feat: ajustando a fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'Poppins, sans-serif'
+      }
+    }
+  },
   plugins: [],
 };
